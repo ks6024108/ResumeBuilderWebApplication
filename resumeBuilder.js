837e3042e71d42be8ce28b6cbf62820133b4a38a
ks6024108 <116474183+ks6024108@users.noreply.github.com>
2023-07-07 12:16:22 +0530

    Update resumeBuilder.js

--- resumeBuilder.js
+++ resumeBuilder.js
@@ -1,31 +1,45 @@
 document.getElementById('resumeForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    generateResume();
-  });
-  
-  function generateResume() {
-    const form = document.getElementById('resumeForm');
-    const preview = document.getElementById('previewContent');
-  
-    preview.innerHTML = `
-      <h3>${form.fullName.value}</h3>
-      <p>Email: ${form.email.value}</p>
-      <p>Phone: ${form.phone.value}</p>
-      <p>Address: ${form.address.value}</p>
-      <h3>Education</h3>
-      <p>${form.educationTitle.value}</p>
-      <p>${form.educationInstitution.value}</p>
-      <p>${form.educationDate.value}</p>
-      <h3>Work Experience</h3>
-      <p>${form.workTitle.value}</p>
-      <p>${form.workCompany.value}</p>
-      <p>${form.workDate.value}</p>
-      <p>${form.workDescription.value}</p>
-      <h3>Skills</h3>
-      <p>${form.skill.value}</p>
-      <!-- Add more fields as needed -->
-    `;
-  
-    document.getElementById('preview').style.display = 'block';
-  }
-  
+  e.preventDefault();
+  generateResume();
+});
+
+function generateResume() {
+  const form = document.getElementById('resumeForm');
+  const preview = document.getElementById('previewContent');
+
+  preview.innerHTML = `
+    <h3 id="form-style">${form.fullName.value}</h3>
+    <p class="styles-form">Email: ${form.email.value}</p>
+    <p class="styles-form">Phone: ${form.phone.value}</p>
+    <p class="styles-form">Address: ${form.address.value}</p>
+    <h3 id="form-style">Education</h3>
+    <p class="styles-form">Degree:${form.educationTitle.value}</p>
+    <p class="styles-form">Instotution:${form.educationInstitution.value}</p>
+    <p class="styles-form">Completion Date:${form.educationDate.value}</p>
+    <h3 id="form-style">Work Experience</h3>
+    <p class="styles-form">Work Title:${form.workTitle.value}</p>
+    <p class="styles-form">Company:${form.workCompany.value}</p>
+    <p class="styles-form">WorkDate:${form.workDate.value}</p>
+    <p class="styles-form"> Work Description:${form.workDescription.value}</p>
+    <h3 id="form-style">Skills</h3>
+    <p class="styles-form">Skills:${form.skill.value}</p>
+    <!-- Add more fields as needed -->
+  `;
+
+  document.getElementById('preview').style.display = 'block';
+}
+
+function download() {
+  debugger
+  var element = document.getElementById('previewContent');
+  // define optional configuration
+  var options = { filename: 'my-file.pdf' };
+
+  var exporter = new html2pdf(element, options);
+  // download the pdf
+  exporter.getPdf(true).then((pdf) => { console.log('pdf downloaded!', pdf); });
+}
+
+
+
+
