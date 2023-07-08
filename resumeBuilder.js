20aee61254680b00567815a14c9c145efe52f334
ks6024108 <116474183+ks6024108@users.noreply.github.com>
2023-07-08 19:09:47 +0530

    Add files via upload

--- resumeBuilder.js
+++ resumeBuilder.js
@@ -47,27 +47,3 @@ function download() {
   exporter.getPdf(true).then((pdf) => { console.log('pdf downloaded!', pdf); });
 }
 
-
-// comment
-{/* <h3 id="form-style">${form.fullName.value}</h3> */}
-
-
-
-// preview.innerHTML = `
-//   <img src="" id="set-image" class="image-set">
-//     <p class="styles-form">Email: ${form.email.value}</p>
-//     <p class="styles-form">Phone: ${form.phone.value}</p>
-//     <p class="styles-form">Address: ${form.address.value}</p>
-//     <h3 id="form-style">Education</h3>
-//     <p class="styles-form">Degree:${form.educationTitle.value}</p>
-//     <p class="styles-form">Instotution:${form.educationInstitution.value}</p>
-//     <p class="styles-form">Completion Date:${form.educationDate.value}</p>
-//     <h3 id="form-style">Work Experience</h3>
-//     <p class="styles-form">Work Title:${form.workTitle.value}</p>
-//     <p class="styles-form">Company:${form.workCompany.value}</p>
-//     <p class="styles-form">WorkDate:${form.workDate.value}</p>
-//     <p class="styles-form"> Work Description:${form.workDescription.value}</p>
-//     <h3 id="form-style">Skills</h3>
-//     <p class="styles-form">Skills:${form.skill.value}</p>
-//     <!-- Add more fields as needed -->
-//   `;
